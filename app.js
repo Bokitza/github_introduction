a49357ec4c9e6b99bf8e13dd9d49cbb457c2e8f2
yoav <sagiyoav@gmail.com>
2024-01-23 19:29:42 +0200

    new button

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("This is the console of my application :P");
+button = document.getElementById("contact");
+button.addEventListener("click", () => {
+  console.log("clicked me!!!");
+});
